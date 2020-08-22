f9680c2da5346585f45bc3908b8089ac532ac4e9
antonylrds <42950733+antonylrds@users.noreply.github.com>
2020-08-21 23:06:56 -0300

    Criar appointment foi adaptado ao typeORM com Postgres

--- src/database/migrations/1598058371273-CreateAppointments.ts
+++ src/database/migrations/1598058371273-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598058371273
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
